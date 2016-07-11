8fe321fe2642344ed281418278f873e978876c2a
Sebastian Sudholt <sebastian.sudholt@tu-dortmund.de>
2016-07-11 12:23:57 +0200

    fixed minor bug

--- src/phocnet/caffe/solver_proto_generator.py
+++ src/phocnet/caffe/solver_proto_generator.py
@@ -4,6 +4,7 @@ Created on Jul 9, 2016
 @author: ssudholt
 '''
 from caffe.proto import caffe_pb2
+from google.protobuf.internal.containers import RepeatedScalarFieldContainer
 
 def generate_solver_proto(**kwargs):
     sp = caffe_pb2.SolverParameter()
@@ -11,5 +12,11 @@ def generate_solver_proto(**kwargs):
         if not hasattr(sp, k):
             raise ValueError('The argument \'%s\' is not part of the Caffe solver parameters!')
         elif v is not None:
-            setattr(sp, k, v)
+            elem = getattr(sp, k)
+            if type(elem) == RepeatedScalarFieldContainer:
+                elem.append(v)
+            elif k == 'solver_mode':
+                setattr(sp, k, sp.SolverMode.DESCRIPTOR.values_by_name[v].number)
+            else:
+                setattr(sp, k, v)
     return sp
