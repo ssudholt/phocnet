651e59572d256db8e4d6ef4ad0e9d56829facce1
Sebastian Sudholt <sebastian.sudholt@tu-dortmund.de>
2016-07-11 12:26:03 +0200

    made exectuable

--- src/phocnet/evaluation/cnn.py
+++ src/phocnet/evaluation/cnn.py
@@ -4,12 +4,12 @@ Created on Jul 10, 2016
 @author: ssudholt
 '''
 import numpy as np
+from phocnet.evaluation.retrieval import map_from_feature_matrix
 
-def calc_map_from_cnn_features(solver, test_iterations, metric='braycurtis'):
-    self.logger.info('Evaluating CNN after %d steps:', epoch*solver.param.test_interval)
+def calc_map_from_cnn_features(solver, test_iterations, metric):    
     net_output = []
     labels = []
-    for test_iter in xrange(solver.param.test_iter[0]):
+    for _ in xrange(solver.param.test_iter[0]):
             # calculate the net output
             solver.test_nets[0].forward()
             
@@ -20,5 +20,7 @@ def calc_map_from_cnn_features(solver, test_iterations, metric='braycurtis'):
     # calculate mAP
     _, ave_precs = map_from_feature_matrix(features=net_output, labels=labels, 
                                            metric=metric, drop_first=True)
+    # some queries might not have a relevant sample in the test set
+    # -> exclude them
     mean_ap = np.mean(ave_precs[ave_precs > 0])
     return mean_ap, ave_precs
