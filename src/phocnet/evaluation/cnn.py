6c4e41b45335264d5efc7893ae38aef39909e1ff
Sebastian Sudholt <sebastian.sudholt@tu-dortmund.de>
2016-07-11 13:41:51 +0200

    corrected shape of label vector

--- src/phocnet/evaluation/cnn.py
+++ src/phocnet/evaluation/cnn.py
@@ -7,16 +7,14 @@ import numpy as np
 from phocnet.evaluation.retrieval import map_from_feature_matrix
 
 def calc_map_from_cnn_features(solver, test_iterations, metric):    
-    net_output = []
-    labels = []
-    for _ in xrange(solver.param.test_iter[0]):
+    net_output = np.zeros((test_iterations, solver.test_nets[0].blobs['sigmoid'].data.flatten().shape[0]))
+    labels = np.zeros(test_iterations)
+    for idx in xrange(solver.param.test_iter[0]):
             # calculate the net output
             solver.test_nets[0].forward()
             
-            net_output.append(solver.test_nets[0].blobs['sigmoid'].data.flatten())
-            labels.append(solver.test_nets[0].blobs['label'].data.flatten())
-    net_output = np.vstack(net_output)
-    
+            net_output[idx] = solver.test_nets[0].blobs['sigmoid'].data.flatten()
+            labels[idx] = solver.test_nets[0].blobs['label'].data.flatten()    
     # calculate mAP
     _, ave_precs = map_from_feature_matrix(features=net_output, labels=labels, 
                                            metric=metric, drop_first=True)
